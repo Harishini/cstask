6b5b75cf1a3c9acfb5592bc125d3709ecf32718f
Harishini <harishini2002@gmail.com>
2022-05-29 21:43:43 +0530

    added files

--- course/settings.py
+++ course/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*yvs-)-#r@!_3^csk_x&=@@jhm^m4m#r-)tfff37665(!3k)by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs20webapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
